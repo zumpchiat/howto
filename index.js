69e998342b134354d870c8d76cd0dd3d30e4ea5a
Renan Z <zumpchiat@gmail.com>
2022-09-19 16:38:45 -0300

    Refatorando controller

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const bodyParser = require('body-parser')
+
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const categorias = require('./routes/categorias')
+
+app.set('views', './views');
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded())
+
 const port = process.env.port || 3000
 
-let i = ''
-const resolver = async (request, response) => {
-    const content = await axios.get(' ')
-    response.render('index', { i: content.data })
-}
 
-app.get('/', resolver)
 
-app.get('/categorias/nova', (req, res) => {
-    res.render('categorias/nova')
-})
+app.get('/', categorias)
 
 app.use('/categorias', categorias)
+
+
 
 app.listen(port, (err) => {
     if (err) {
--- routes/categorias.js
+++ routes/categorias.js
@@ -1,38 +1,18 @@
-const api = require('../api')
 const express = require('express')
 const router = express.Router()
-
-router.post('/nova', async (req, res) => {
-    await api.create('categorias', {
-        categorias: req.body.categorias
-    })
-    res.redirect('/categorias')
-})
-
-router.get('', async (req, res) => {
-    const categorias = await api.list('categorias')
-    res.render('categorias/index', { categorias })
-})
-
-router.get('/excluir/:id', async (req, res) => {
-    await api.apagar('categorias', req.params.id)
-    res.redirect('/categorias')
-})
+const controller = require('../controllers/categorias')
 
 
-router.get('/editar/:id', async (req, res) => {
-    const categoria = await api.get('categorias', req.params.id)
-    res.render('categorias/editar', {
-        categoria
-    })
-})
 
-router.post('/editar/:id', async (req, res) => {
-    await api.update('categorias', req.params.id, {
-        categorias: req.body.categorias
-    })
-    res.redirect('/categorias')
-})
+router.get('/nova', controller.novaForm)
+router.post('/nova', controller.nova)
 
+router.get('/', controller.list)
+
+router.get('/excluir/:id', controller.excluir)
+
+router.get('/editar/:id', controller.editarForm)
+
+router.post('/editar/:id', controller.editar)
 
 module.exports = router
